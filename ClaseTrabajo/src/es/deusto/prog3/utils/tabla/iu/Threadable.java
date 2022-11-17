cad2634496dd73fb968142c31a4ff8e38337986c
IkerCubillo <i.cubillo@opendeusto.es>
2022-11-17 18:46:44 +0100

    Unicamente actividades realizadas

--- ClaseTrabajo/src/es/deusto/prog3/utils/tabla/iu/Threadable.java
+++
@@ -1,7 +0,0 @@
-package es.deusto.prog3.utils.tabla.iu;
-
-/** Interfaz Runnable cuyo método run se ejecutará en un hilo independiente
- * @author andoni.eguiluz @ ingenieria.deusto.es
- */
-public interface Threadable extends Runnable {
-}
