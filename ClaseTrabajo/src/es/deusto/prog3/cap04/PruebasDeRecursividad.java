c95295a8d61ea8eab3f85c19869f2b70197cc543
i.cubillo <i.cubillo@E105L4.opendeusto.es>
2022-11-23 11:56:47 +0100

    fibo

--- ClaseTrabajo/src/es/deusto/prog3/cap04/PruebasDeRecursividad.java
+++ ClaseTrabajo/src/es/deusto/prog3/cap04/PruebasDeRecursividad.java
@@ -2,16 +2,31 @@ package es.deusto.prog3.cap04;
 
 public class PruebasDeRecursividad {
 	
-	private static int numLlamadas;
+	private static long numLlamadas;
 	
 	public static void main(String[] args) {
-//		numLlamadas = 0;
+		numLlamadas = 0;
 //		int varPrueba = 7;
 //		// f( varPrueba );
 //		conteo( 1 );
-		System.out.println(factorial(35));
+//		System.out.println(factorial(35));
+		System.out.println(fib(46));
+		System.out.println(numLlamadas);
 	}
 	
+	// si n > 1, fib (n) = fib(n-1) + fib(n-2)
+	// si n = 1, fib(1) = 1
+	private static long fib(long n) {
+		numLlamadas++;
+		if (n == 1) { // Caso base
+			return 1;
+		} else if (n==2) {
+			return 1;
+		} else { // Caso recursivo
+			return fib(n-1) + fib(n-2); 
+		}
+	}
+
 	// n! = 1 * 2 * 3 * 4 * 5 * ... * (n-2) * (n-1) * n
 	// si n > 1 --> n! = n * (n-1)! [factorial(n) returns n * factorial(n-1)]
 	//	 n == 0 --> n! = 1
@@ -38,7 +53,7 @@ public class PruebasDeRecursividad {
 		}
 	}
 	
-	private static void f( int param ) {
+	private static void f( long param ) {
 		param = param + numLlamadas;
 		numLlamadas++;
 		System.out.println( "soy f en la llamada " + numLlamadas + " cálculo " + param );
